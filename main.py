8ebd0e17014800e849dd06bf8cd2a39a020c6a81
Randy F <877wrangler@gmail.com>
2023-02-08 01:46:03 -0500

    Add index stock table and stock_detail table

--- main.py
+++ main.py
@@ -12,11 +12,27 @@ def index(request: Request):
     cursor = connection.cursor()
 
     cursor.execute("""
-        SELECT id, symbol, name FROM stock 
+        SELECT id, symbol, name FROM stock ORDER BY symbol
     """)
 
     rows = cursor.fetchall()
 
-    return templates.TemplateResponse("index.html", {"request": request, "id": id})
+    return templates.TemplateResponse("index.html", {"request": request, "stocks": rows})
 
-    return {"Hello": "World", "stocks": rows}
+@app.get("/stock/{symbol}")
+def stock_detail(request: Request, symbol):
+    connection = sqlite3.connect(config.DB_FILE)
+    connection.row_factory = sqlite3.Row
+    cursor = connection.cursor()
+
+    cursor.execute("""
+        SELECT id, symbol, name FROM stock WHERE symbol = ?
+    """, (symbol,))
+
+    row = cursor.fetchone()
+    cursor.execute("""
+        SELECT * FROM stock_price WHERE stock_id = ?
+    """, (row['id'],))
+    prices = cursor.fetchall()
+    
+    return templates.TemplateResponse("stock_detail.html", {"request": request, "stock": row, "bars": prices})
