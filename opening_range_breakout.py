02b4c0bf83671de7f0d5c36a0dc2f43c2b6e1b2c
877wrangler <877wrangler@gmail.com>
2023-02-08 22:53:47 -0500

    Set up opening range breakout auto-trade

--- opening_range_breakout.py
+++ opening_range_breakout.py
@@ -1,7 +1,11 @@
 import sqlite3
 import config
 import datetime as date
-from yfinanceapi import today_first_15min
+from yfinanceapi import today_first_15min, get_daily_minbars
+import alpaca_trade_api as tradeapi
+from alpaca_trade_api.rest import REST
+
+api = tradeapi.REST(config.API_KEY, config.SECRET_KEY, base_url=config.API_URL)
 
 connection = sqlite3.connect(config.DB_FILE)
 connection.row_factory = sqlite3.Row
@@ -19,14 +23,49 @@ cursor.execute("""
 
 stocks = cursor.fetchall()
 symbols = [stock['symbol'] for stock in stocks]
-print(symbols)
+# print(symbols)
+
+orders = api.list_orders()
+existing_order_symbols = [order.symbol for order in orders]
+
 
 for symbol in symbols:
     opening_range_bars = today_first_15min(symbol)
     opening_range_low = opening_range_bars['Low'].min()
     opening_range_high = opening_range_bars['High'].max()
     opening_range = opening_range_high - opening_range_low
-    print(opening_range_low)
-    print(opening_range_high)
-    print(opening_range)
+
+    after_opening_mins = get_daily_minbars(symbol)
+
+    # print(opening_range_low)
+    # print(opening_range_high)
+    # print(opening_range)
+
+    after_opening_range_breakout = after_opening_mins[after_opening_mins['Close'] > opening_range_high]
+    # print(after_opening_range_breakout)
+
+    if not after_opening_range_breakout.empty:
+        if symbol not in existing_order_symbols:
+            limit_price = round(after_opening_range_breakout.iloc[0]['Close'], 2)
+            trade_time = after_opening_range_breakout.index[0].time().strftime("%H:%M:%S")
+            print(f"Placing order for {symbol} at {limit_price}, closed above {opening_range_high} at {trade_time}")
+
+            api.submit_order(
+                symbol=symbol,
+                side='buy',
+                type='limit',
+                qty=100,
+                time_in_force='day',
+                order_class='bracket',
+                limit_price=limit_price,
+                take_profit=dict(
+                    limit_price=round(limit_price + opening_range, 2),
+                ),
+                stop_loss=dict(
+                    stop_price=round(limit_price - opening_range,2),
+                    limit_price=round(limit_price - opening_range, 2),
+                )
+            )
+        else:
+            print(f"Already an order for {symbol}, skipping")
 
--- yfinanceapi.py
+++ yfinanceapi.py
@@ -5,9 +5,19 @@ def today_first_15min(ticker):
     # pd.options.display.max_rows=10  # To decrease printouts
 
     current_date = pendulum.now().date()
+
     start = pendulum.datetime(current_date.year, current_date.month, current_date.day, 9, 30, tz='America/New_York')
     end = pendulum.datetime(current_date.year, current_date.month, current_date.day, 9, 45, tz='America/New_York')
 
     data = yf.download(tickers=ticker, interval="1m", start=start, end=end)
     return data
 
+def get_daily_minbars(ticker):
+    current_time = pendulum.now()
+
+    start = pendulum.datetime(current_time.year, current_time.month, current_time.day, 9, 45, tz='America/New_York')
+    end = pendulum.datetime(current_time.year, current_time.month, current_time.day, current_time.hour, current_time.minute, tz='America/New_York')
+    data = yf.download(tickers=ticker, interval="1m", start=start, end=end)
+    return data
+
+# get_daily_minbars('VKTX')
